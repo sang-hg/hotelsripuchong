e5eff3c4201d86fd5d18cffda31b668270e84f10
sang-hg <sang.hmung@mail.com>
2025-03-09 12:51:10 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class hspheader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <header id="banner">
-      <a href="index.html"><img src="sphPhotos/headbanner.jpg" alt="sri puchong logo"  style="width: 90%;"></a>
+      <a href="index.html"><img src="headbanner.jpg" alt="sri puchong logo"  style="width: 90%;"></a>
     </header>
     `
   }
@@ -23,7 +23,7 @@ class hspnavbar extends HTMLElement {
     this.innerHTML = `
     <nav class="navbar">
       <ul id="navmenu">
-        <li><a class="navicon" onclick="myFunction()"><img src="sphPhotos/icons8-menu-bar-32.png"></a></li>
+        <li><a class="navicon" onclick="myFunction()"><img src="icons8-menu-bar-32.png"></a></li>
         <li><a href="index.html">Home</a></li>
         <li><a href="gallery.html"> Gallery</a></li>
         <li><a href="reservation.html">Reservation</a></li>
@@ -58,9 +58,9 @@ class hspfooter extends HTMLElement {
           <li></li>
         </ul>
         <ul>
-          <li><a href="https://www.facebook.com/people/Hotel-Sri-Puchong/100069287625990/#" target="_blank"><img alt="icon facebook" src="sphPhotos/icons8-facebook-32.png"></a></li>
-          <li><img src="sphPhotos/icons8-instagram-32.png" alt="instagram icon"></li>
-          <li><img src="sphPhotos/icons8-tiktok-32.png" alt="tiktok icon"></li>
+          <li><a href="https://www.facebook.com/people/Hotel-Sri-Puchong/100069287625990/#" target="_blank"><img alt="icon facebook" src="icons8-facebook-32.png"></a></li>
+          <li><img src="icons8-instagram-32.png" alt="instagram icon"></li>
+          <li><img src="icons8-tiktok-32.png" alt="tiktok icon"></li>
         </ul>
       </section>
       <section class="footer">
